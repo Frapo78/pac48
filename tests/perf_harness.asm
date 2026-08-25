a5054c3b7f3bfdbbfa6632412f909fc4d10265dc
Fra <40823974+Frapo78@users.noreply.github.com>
2026-08-25 04:32:26 +0200

    P48-025: include pellet gameplay module in perf harness

--- tests/perf_harness.asm
+++ tests/perf_harness.asm
@@ -20,6 +20,7 @@
         INCLUDE "../src/sprites.asm"
         INCLUDE "../src/generated/pac_shifted.asm"
         INCLUDE "../src/maze.asm"
+        INCLUDE "../src/pellets.asm"
         INCLUDE "../src/player.asm"
         INCLUDE "../src/render.asm"
 
