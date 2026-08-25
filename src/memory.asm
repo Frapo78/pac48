865aefe5fc234d58b1a04d689424196c91962a29
Fra <40823974+Frapo78@users.noreply.github.com>
2026-08-25 02:55:57 +0200

    P48-010 P48-012: add renderer state and line lookup memory

--- src/memory.asm
+++ src/memory.asm
@@ -14,3 +14,28 @@ Pac_Dir:       DB 0
 Pac_ReqDir:    DB 0
 
 Input_Mode:    DB 0   ; 0=Q/A/O/P, 1=Kempston, 2=Sinclair 1, 3=Sinclair 2
+
+; ------------------------------------------
+; Renderer persistent/prepared state
+; ------------------------------------------
+Render_PlayerX:        DB 24
+Render_PlayerY:        DB 24
+Render_PlayerPhase:    DB 0
+Render_PlayerSprite:   DW 0
+
+Render_DirtyCount:     DB 0
+Render_NextDirtyCount: DB 0
+Render_DirtyCells:     DEFS 48, 0      ; 24 x/y cell pairs
+Render_NextDirtyCells: DEFS 48, 0      ; 24 x/y cell pairs
+
+; Renderer scratch used only inside prepare/commit routines.
+Render_TileX:          DB 0
+Render_TileY:          DB 0
+Render_CrossX:         DB 0
+Render_CrossY:         DB 0
+Render_DrawXByte:      DB 0
+Render_DrawY:          DB 0
+
+; 192 Spectrum bitmap scanlines x 16-bit address.
+; Initialized once by Video_InitLineTable during startup.
+Video_LineAddrTable:   DEFS 384, 0
